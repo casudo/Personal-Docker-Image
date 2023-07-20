bcb58725843516ae3b896d5f373bb28d6688beee
Casudo <sudfwd+github@gmail.com>
2023-07-20 20:32:12 +0200

    Updated the business card

--- src/script.py
+++ src/script.py
@@ -17,7 +17,7 @@ class color:
 # Print he business card
 tprint("Casudo")
 
-print(color.BOLD + "Hello, my name is " + color.GREEN + "Kida ✌\n" + color.END)
+print(color.BOLD + "Hello, my name is " + color.GREEN + "Kida! :)\n" + color.END)
 print("This is my virtual business card.\n")
 
 print(color.BOLD + color.YELLOW + "E-Mail:" + color.END + " contact@k1da.de")
@@ -30,4 +30,4 @@ print(color.BOLD + color.BLUE + "LinkedIn:" + color.END + " Please ask me direct
 print(color.UNDERLINE + color.DARKCYAN + "2048-Bit RSA public key:" + color.END)
 print("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoIAKZnv/YDclt0B0Sk1YO6QD430J5e6TzGIo3hUYN9/abPdb9Sdm4phiHScIQq7X5jN2PkN6pM25OwKmqrUJ7vBUScZlZahkP+gbWpk836Te8CX1/jAYqtEBWQsbUSUvFVwX8xOJOk8NwBE7OhQ0GudNi/wNbtkLEaAYYhsad+nbEasrlKE27ub6MMFW2U6QJ/aG1w+edT0ofizClOF2Vm2skKBU+jmz+xAVexntjL5jbgfO5moZrp7lLiyVn5Gz2fbyu/36Y6TXm36/0ZJ0epMzG1mDjSOPQSd/rv8vx7mse5BZli0+M8SbruiSJTtRSqAjEWo7j6E1BVIslxjpkwIDAQAB")
 
-print("\n© k1da 2023\n")
+print("\n© k1da 2023")
